adc2b70644b1de2213721f33f10e7cb1ae45f44c
EternalMelody <mario.andika@gmail.com>
2014-08-07 23:59:30 -0700

    Applied Holo theme.

--- DeployApp/src/com/kidgeniusdesigns/deployapp/CreateEvent.java
+++ DeployApp/src/com/kidgeniusdesigns/deployapp/CreateEvent.java
@@ -23,6 +23,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
@@ -70,6 +71,7 @@ public class CreateEvent extends FragmentActivity implements OnItemClickListener
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_create_event);
 		CreateEvent.tilEvent = Calendar.getInstance();
 		titleHints= new String[5];
--- DeployApp/src/com/kidgeniusdesigns/realdeploy/SplashScreen.java
+++ DeployApp/src/com/kidgeniusdesigns/realdeploy/SplashScreen.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
 
 import com.kidgeniusdesigns.deployapp.MainActivity;
 
@@ -15,6 +16,7 @@ public class SplashScreen extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash_screen);
  
         new Handler().postDelayed(new Runnable() {
